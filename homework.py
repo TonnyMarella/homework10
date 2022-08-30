504c4e248e677410038e0536430af8e919b3439b
KapOo <temazubkov02@gmail.com>
2022-08-30 22:46:02 +0300

    Update

--- homework.py
+++ homework.py
@@ -24,18 +24,26 @@ class Phone(Field):
 
 
 class Record(Field):
-    def __init__(self):
-        self.name = Name
-        self.phone = Phone
+    def __init__(self, new_name):
+        self.name = Name(new_name)
+        self.phone = []
 
-    def add_contact(self, new_name, new_phone):
-        self.name = self.name(new_name)
-        self.phone = self.phone(new_phone)
+    def add_contact(self, new_phone):
+        self.phone.append(Phone(new_phone))
 
     def edit_contact(self, cls, contact_name, new_phone):
         if contact_name in cls.data:
             self.name = self.name(contact_name)
             self.phone = self.phone(new_phone)
+
+
+def get_name_and_phone():
+    """
+    To avoid duplication
+    """
+    name = input('Enter name:\n')
+    phone = input('Enter one phone number: \n')
+    return name, phone
 
 
 def main():
@@ -54,31 +62,37 @@ def main():
             print(adressbook.data)
         elif a == 'show':  # Show one contact
             name = input('Enter name:\n')
-            print(adressbook.data[name].name.value, 'phone:', adressbook.data[name].phone.value)
+            print('name:', adressbook.data[name].name.value, 'phone:',
+                  list(map(lambda x: x.value, adressbook.data[name].phone)))
         elif a.split()[0] == 'add':  # Add contact
-            name = input('Enter name:\n')
-            phone = input('Enter phone: \n')
-            if name and phone:
-                record_add = Record()
-                record_add.add_contact(name.lower(), phone.split() if ' ' in phone else [phone])
-                adressbook.add_record(record_add)
+            name, phone = get_name_and_phone()
+            if len(phone.split()) > 1:
+                print('Enter ONE phone number')
             else:
-                print('Enter correct name and phone')
+                if name and phone:
+                    record_add = Record(name.lower())
+                    record_add.add_contact(phone)
+                    adressbook.add_record(record_add)
+                else:
+                    print('Enter correct name and phone')
         elif a.split()[0] == 'change':  # Add contact number
-            name = input('Enter name:\n')
-            phone = input('Enter phone: \n')
+            name, phone = get_name_and_phone()
             try:
-                adressbook.data[name].phone.value.append(phone)
+                record_change = adressbook.data[name]
+                record_change.add_contact(phone)
+                adressbook.add_record(record_change)
             except KeyError:
                 print('Wrong name entered')
         elif a.split()[0] == 'delete':  # Delete contact number
-            name = input('Enter name:\n')
-            phone = input('Enter phone which you want to delete: \n')
+            name, phone = get_name_and_phone()
             try:
-                adressbook.data[name].phone.value.remove(phone)
+                record_delete = adressbook.data[name]
+                need_to_delete = list(filter(lambda x: x.value == phone, record_delete.phone))
+                record_delete.phone.remove(need_to_delete[0])
+                adressbook.add_record(record_delete)
             except KeyError:
                 print('Wrong name entered')
-            except ValueError:
+            except IndexError:
                 print('The phone number not exist')
 
 
